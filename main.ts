833c389ef2d8b1546fbbe93c5a47e6b4784b88b2
Kilian Funk <45737500+kilian-funk@users.noreply.github.com>
2021-06-15 08:30:21 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (input.compassHeading() < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() < 135) {
+        basic.showArrow(ArrowNames.West)
+    } else if (input.compassHeading() < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() < 305) {
+        basic.showArrow(ArrowNames.East)
+    } else {
+        basic.showArrow(ArrowNames.North)
+    }
+})
